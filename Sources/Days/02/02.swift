6562d73abc794e67d208bc15d6afd0646d690e8e
Simon Erlic <simon@erlic.ca>
2024-12-01 22:42:52 -0800

    Day 2!!!

--- Sources/Days/02/02.swift
+++ Sources/Days/02/02.swift
@@ -4,22 +4,73 @@ public class Day02: Day {
     private let input: String
 
     required init() {
-        self.input = FileReader.readInput(for: 1)
+        self.input = FileReader.readInput(for: 2)
     }
 
     required init(input: String? = nil) {
-        self.input = input ?? FileReader.readInput(for: 1)
+        self.input = input ?? FileReader.readInput(for: 2)
     }
 
     static func withTestInput() -> Self {
-        return self.init(input: FileReader.readInput(for: 1, type: .test))
+        return self.init(input: FileReader.readInput(for: 2, type: .test))
     }
 
     func solvePart1() -> String {
-        return "Not implemented yet"
+        var sum = 0
+        let lines = input.split(separator: "\r\n")
+
+        for line in lines {
+            let levels = line.split(separator: " ").compactMap { Int($0) }
+            print(levels)
+            if isSafe(levels: levels) {
+                sum += 1
+            }
+        }
+
+        return String(sum)
     }
 
     func solvePart2() -> String {
-        return "Not implemented yet"
+        var sum = 0
+        let lines = input.split(separator: "\r\n")
+
+        for line in lines {
+            let levels = line.split(separator: " ").compactMap { Int($0) }
+            if isSafe(levels: levels) || dampenerCheck(levels: levels) {
+                sum += 1
+            }
+        }
+
+        return String(sum)
+    }
+
+    func isSafe(levels: [Int]) -> Bool {
+        guard levels.count > 1 else { return true }
+
+        var isIncreasing: Bool? = nil
+        for i in 1..<levels.count {
+            let diff = levels[i] - levels[i - 1]
+            if diff == 0 || abs(diff) > 3 {
+                return false
+            }
+            if isIncreasing == nil {
+                isIncreasing = diff > 0
+            } else if (isIncreasing! && diff < 0) || (!isIncreasing! && diff > 0) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func dampenerCheck(levels: [Int]) -> Bool {
+        for i in 0..<levels.count {
+            var modifiedLevels = levels
+            modifiedLevels.remove(at: i)
+            if isSafe(levels: modifiedLevels) {
+                return true
+            }
+        }
+        return false
     }
 }
--- Sources/Days/03/03.swift
+++ Sources/Days/03/03.swift
@@ -4,15 +4,15 @@ public class Day03: Day {
     private let input: String
 
     required init() {
-        self.input = FileReader.readInput(for: 1)
+        self.input = FileReader.readInput(for: 3)
     }
 
     required init(input: String? = nil) {
-        self.input = input ?? FileReader.readInput(for: 1)
+        self.input = input ?? FileReader.readInput(for: 3)
     }
 
     static func withTestInput() -> Self {
-        return self.init(input: FileReader.readInput(for: 1, type: .test))
+        return self.init(input: FileReader.readInput(for: 3, type: .test))
     }
 
     func solvePart1() -> String {
