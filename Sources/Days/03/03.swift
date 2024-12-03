e62d319fa96d8d5b1ee5d5c6df5d3b9beb6f79c7
Simon Erlic <simon@erlic.ca>
2024-12-02 21:23:34 -0800

    Day three!! The part 2 test eval doesn't work correctly, gotta fix that

--- Sources/Days/03/03.swift
+++ Sources/Days/03/03.swift
@@ -16,10 +16,41 @@ public class Day03: Day {
     }
 
     func solvePart1() -> String {
-        return "Not implemented yet"
+        return calculateSum(using: "mul\\((\\d+),(\\d+)\\)")
     }
 
     func solvePart2() -> String {
-        return "Not implemented yet"
+        return calculateSum(using: "mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+    }
+
+    private func calculateSum(using pattern: String) -> String {
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let nsString = input as NSString
+        let matches = regex.matches(in: input, range: NSRange(location: 0, length: nsString.length))
+
+        var sum = 0
+        var isEnabled = true
+
+        for match in matches {
+            let matchString = nsString.substring(with: match.range)
+
+            if matchString == "do()" {
+                isEnabled = true
+            } else if matchString == "don't()" {
+                isEnabled = false
+            } else if match.numberOfRanges == 3 {
+                let xRange = match.range(at: 1)
+                let yRange = match.range(at: 2)
+
+                if let x = Int(nsString.substring(with: xRange)),
+                    let y = Int(nsString.substring(with: yRange)),
+                    isEnabled
+                {
+                    sum += x * y
+                }
+            }
+        }
+
+        return String(sum)
     }
 }
